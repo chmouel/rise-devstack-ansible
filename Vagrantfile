b3cfe413390c5cee00f27b97763049bcd8307704
Chmouel Boudjnah <chmouel@chmouel.com>
2014-03-17 15:01:29 +0100

    Rise it!

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,27 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "saucy"
-  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/raring/current/raring-server-cloudimg-amd64-vagrant-disk1.box"
+# -*- Mode: ruby -*-
 
+Vagrant.configure("2") do |config|
   config.ssh.forward_agent = true
   config.vm.provision :ansible, :playbook => "rise.yaml"
-end
 
+  config.vm.provider :virtualbox do |vb, override|
+    override.vm.box = "saucy"
+    override.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/raring/current/raring-server-cloudimg-amd64-vagrant-disk1.box"
+  end
+
+  config.vm.provider :openstack do |os, override|
+    override.vm.box = "dummy"
+    override.vm.box_url = "https://github.com/cloudbau/vagrant-openstack-plugin/raw/master/dummy.box"
+    override.ssh.username = "ubuntu"
+    override.ssh.private_key_path = "~/.ssh/id_rsa-nopasswd"
+
+    os.server_name = "devstack3.chmouel.com"
+    os.username     = "#{ENV['OS_USERNAME']}"
+    os.api_key      = "#{ENV['OS_PASSWORD']}"
+    os.flavor       = /m1.tiny/
+    os.image        = /Ubuntu 13.10 x86_64/
+    os.endpoint     = "#{ENV['OS_AUTH_URL']}/tokens"
+    os.keypair_name = "Nopasswd"
+    os.floating_ip  = "46.231.128.152"
+  end
+end
---
+++ roles/riseit/files/devstack-local-shell-config.zsh
@@ -0,0 +1,72 @@
+# -*- Mode: shell-script -*-
+userColor="green"
+hostColor="green"
+
+export UK_RCLOUD_USER=@UK_RCLOUD_USER@
+export UK_RCLOUD_KEY=@UK_RCLOUD_KEY@
+export UK_RCLOUD_AURL=https://lon.auth.api.rackspacecloud.com/v1.0
+
+alias -g MJ="|python -mjson.tool"
+alias -g SP="|curl -F 'sprunge=<-' http://sprunge.us"
+alias so_os=soso
+alias so_os_admin="soso admin admin"
+OS_TENANT_NAME=swifttenanttest1 OS_USERNAME=swiftusertest1 OS_PASSWORD=testing
+alias so_os_testuser="so_os;export OS_TENANT_NAME=swifttenanttest1 OS_USERNAME=swiftusertest1 OS_PASSWORD=testing"
+alias so_euc="pushd ~/devstack >/dev/null && source eucarc && popd >/dev/null"
+alias inst="sudo apt-get -y install"
+alias remove="sudo apt-get -y remove"
+alias u="upcs -cpublic"
+
+alias swiftv1='ST_USER=admin:admin ST_AUTH="http://localhost:8080/auth/v1.0" ST_KEY=admin swift stat'
+
+
+function soso {
+    eval $(for i in $(env|grep OS_);do echo -n "export ";echo -n ${i%%=*};echo =;done)
+    pushd ~/devstack >/dev/null && {
+        eval $(bash -c ". openrc $1 $2;env|sed -n '/OS_/ { s/^/export /;p}'")
+    } && popd >/dev/null
+}
+soso
+
+function gd() {
+    arg=$1
+
+    if [[ ${arg} == http* ]];then
+        [[ ${arg} == */c/* ]] && arg=$(echo ${arg}|sed 's,.*/c/,,;s,/.*,,')
+        [[ ${arg} =~ "/([0-9]{4,})$" ]] && arg=${MATCH[2,-1]}
+    fi
+    if [[ ${arg} =~ "^[0-9]{4,}$" ]];then
+        :
+    else
+        echo "I could not figure out what review this is: ${arg}"
+        return 1
+    fi
+    git-review -d ${arg}
+}
+
+function os_complete {
+    local comp=$1
+    local d=/opt/stack/python-${1}client/tools/${comp}.bash_completion
+
+    [[ -e ${d} ]] || { echo "No project ${comp}"; return 1;}
+
+    autoload -U bashcompinit
+    bashcompinit
+    source ${d}
+
+}
+
+function vin() { vim $(fin $1) ;}
+function rejoin_stack() {
+     mount | grep -q /opt/stack/data/swift/drives/images/swift.img || \
+         sudo mount -t xfs -o loop,noatime,nodiratime,nobarrier,logbufs=8 \
+            /opt/stack/data/swift/drives/images/swift.img  /opt/stack/data/swift/drives/sdb1
+    mkdir -p /tmp/screen-logs
+    sed -i '/^hardstatus/ { s/.*/source ~\/.screenrc\n/}' ~/devstack/stack-screenrc
+    cd ~/devstack
+    ./rejoin-stack.sh
+}
+
+unset USERNAME
+
+[[ -e /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh
